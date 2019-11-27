f3072222b4f058dcdeba28b55744b362d5dc939f
baturinsky <baturinsky@gmail.com>
2019-11-27 05:04:50 +0500

    Cloning into Marshmallow repo

---
+++ src/index.ts
@@ -0,0 +1,289 @@
+import { canvas, prepareRender } from "./render";
+import Keyboard from "./Keyboard";
+import { m4, v3 } from "./twgl/twgl-full";
+import { Vec3 } from "./twgl/v3";
+import zzfxInit from "./zzfx";
+import { V2, length } from "./v2";
+
+let volume = 1;
+
+let sfxDefs = {
+  playerExplode: [10,.1,0,.8,.05,.7,4.6,21.2,.4],
+  explode: [30,.1,1811,.8,.01,1.9,3,.5,.04],
+  powerup: [10,.1,240,.2,.85,5.7,.8,0,.77],
+  shieldDest: [10,.1,1750,.7,.05,.1,1.8,.3,.18],
+  blast: [10,.1,5000,1.7,.01,0,4,0,.45],
+
+  ponk:[.5, .1, 1549, .1, 0, 0, 1.2, 48.9, .17],
+  
+  //UNUSED BUT COOL
+  boom: [10,.1,3000,1.5,.01,0,4,0,.48],		
+  rumble: [10,.1,200,.7,.1,0,5,.1,.36],
+  sad: [10,.1,264,1.1,.53,0,0,.1,.94],
+  vwom: [10,.1,1,1,.11,0,0,69.2,.95],
+  pop: [10,.1,21,.1,.46,9,.1,5.7,.12],
+  swipe: [10,.1,866,.1,.5,0,1.6,.4,.98],
+  ow: [10,.1,1682,.2,.66,3,0,1.2,.69],
+  wave: [10,.1,1825,1,.2,0,5,.2,.3],
+  bobbleUp: [1,.1,17,1,.26,.2,.1,8,.66],
+};
+
+let zzfx:Function;
+
+function sfx(def:string){
+  if(!zzfx)
+    zzfx = zzfxInit();
+  zzfx(sfxDefs[def][0] * volume, ...sfxDefs[def].slice(1))
+}
+
+type Memory = {
+  pos: Vec3;
+  time: number;
+  orb: number;
+  vel: number;
+  rot: [number, number];
+};
+
+let rad = Math.PI / 180;
+
+let pos: Vec3;
+let vel: number;
+let rot: V2
+let mouseDelta: V2;
+let smoothRot: V2;
+let time = 0;
+let collected = new Uint8Array(1000);
+let collectedAmount = 0;
+let history: Memory[] = [];
+//const initialPos = [600, 300, 250];
+const initialPos = [0, 0, 250];
+const initialVel = 30;
+const acc = 300;
+const heightToSpeed = 0.3;
+const drag = 0.03;
+const orbSpeedBonus = 10;
+const minimumVelocity = 10;
+const accPerClick = 30;
+const slowPerClick = 30;
+const orbsToCollect = 100;
+
+function active() {
+  return document.pointerLockElement == canvas && !won();
+}
+
+function won() {
+  return collectedAmount >= orbsToCollect;
+}
+
+function eject() {
+  collectedAmount = Math.max(0, collectedAmount - 5);
+  if (collectedAmount == 0) time = 0;
+  pos[2] = 300;
+  vel = initialVel;
+}
+
+function crash() {
+  console.log("crash");
+  sfx("bobbleUp");
+  //rewind();
+  eject();
+}
+
+function remember(orb = -1) {
+  history.push({ pos, time, orb, vel, rot });
+}
+
+function collect(orb: number) {
+  if (collected[orb]) return;
+  sfx("ponk");
+  collectedAmount++;
+  vel += orbSpeedBonus;
+  if (orb >= 0) collected[orb] = 1;
+  history.push({ pos, time, orb, vel, rot });
+}
+
+function fract(n) {
+  return n - Math.floor(n);
+}
+
+function hash(p: [number, number]) {
+  return fract(
+    1e4 *
+      Math.sin(17.0 * p[0] + p[1] * 0.1) *
+      (0.1 + Math.abs(Math.sin(p[1] * 13.0 + p[0])))
+  );
+}
+
+const penaltySteps = 3;
+
+function rewind(steps = -1) {
+  if (steps < 0) steps = penaltySteps;
+  let memoriesRemained = Math.max(1, history.length - penaltySteps);
+  history = history.slice(0, memoriesRemained);
+  let lastMemory = history[history.length - 1];
+  collected = new Uint8Array(1000);
+  collectedAmount = 0;
+
+  for (let m of history) {
+    if (m.orb >= 0) {
+      collectedAmount++;
+    }
+    collected[m.orb] = 1;
+  }
+  pos = lastMemory.pos;
+  time = lastMemory.time;
+  vel = lastMemory.vel;
+  rot = lastMemory.rot;
+  smoothRot = rot.slice() as [number, number];
+  mouseDelta = [0,0];
+}
+
+window.onload = async e => {
+  let renderHQ = await prepareRender(crash, collect, collected, 1);
+  let renderLQ = await prepareRender(crash, collect, collected, 0.5);
+
+  let render = renderHQ;
+
+  let statsDiv = document.getElementById("stats");
+  let orbsDiv = document.getElementById("orbs");
+  let pauseDiv = document.getElementById("pause");
+  let winDiv = document.getElementById("win");
+
+  //canvas.requestPointerLock();
+  const keyboard = new Keyboard(document);
+
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (e.code == "Space") {
+      if (!active()) canvas.requestPointerLock();
+      else document.exitPointerLock();
+    }
+
+    if (e.code == "KeyR") {
+      rewind(1e6);
+      return;
+    }
+
+    if (e.code == "KeyQ") {
+      render = render==renderHQ?renderLQ:renderHQ;
+      return;
+    }
+
+  });
+
+  canvas.addEventListener("mousedown", e => {
+    if (!active()) canvas.requestPointerLock();
+    else {
+      if (e.button == 0 && collectedAmount > 0) {
+        collectedAmount--;
+        vel += accPerClick;
+      }
+      if (e.button == 2) {
+        vel = Math.max(vel - slowPerClick, minimumVelocity * 2);
+      }
+    }
+  });
+
+  canvas.addEventListener("mousemove", (e: MouseEvent) => {
+    mouseDelta[0] += e.movementX;
+    mouseDelta[1] += e.movementY;
+  });
+
+  let lastTime = 0;
+
+  vel = initialVel;
+  pos = initialPos;
+  rot = [0, 0];
+  smoothRot = [0, 0];
+  mouseDelta = [0, 0];
+  let dir: Vec3 = [1, 0, 0];
+
+  remember();
+  //rewind();
+
+  let loops = 0;
+
+  function loop(frameTime: number) {
+    pauseDiv.style.visibility = !active() && !won() ? "visible" : "hidden";
+
+    winDiv.style.visibility = won() ? "visible" : "hidden";
+    if (won()) {
+      winDiv.innerHTML = `<h2>Congaturaleishuns!</h2> You have collected <b>${collectedAmount}</b> orbs in <b>${Math.floor(
+        time
+      )} seconds!</b><br/><br/> Press <b>R</b> to restart.`;
+      return;
+    }
+
+    if (!active() && loops > 0) {
+      window.requestAnimationFrame(loop);
+      return;
+    }
+
+    let dTime = (frameTime - lastTime) / 1000;
+
+    if (dTime > 0.1) dTime = 0.1;
+
+    lastTime = frameTime;
+
+    time += dTime;
+
+    mouseDelta = mouseDelta.map(d => Math.sign(d) * Math.min(30, Math.abs(d) * dTime * 60)) as V2;
+
+    //vel -= length(mouseDelta) * 0.01;
+
+    rot[0] = rot[0] - mouseDelta[0] * 0.1;
+    rot[1] = Math.max(-90, Math.min(90, rot[1] - mouseDelta[1] * 0.1));
+
+    //lastRot = [0,0];
+
+    let turn = Math.min(1, dTime * 30)
+
+    smoothRot = smoothRot.map((prevSmooth, i) => prevSmooth * (1 - turn) + rot[i] * turn ) as V2;
+
+    //smoothRot = rot.slice() as V2;
+
+    mouseDelta = [0,0];
+
+    /*console.log("-");
+    console.log(...mouseDelta);
+    console.log(...rot);*/
+
+    let [yaw,pitch] = smoothRot.map(v => v * rad);
+
+    dir = v3.normalize([
+      Math.cos(pitch) * Math.cos(yaw),
+      Math.cos(pitch) * Math.sin(yaw),
+      Math.sin(pitch)
+    ]);
+
+    mouseDelta = [0,0];
+
+    if (keyboard.pressed["KeyO"]) {
+      vel += acc * dTime;
+    }
+
+    if (keyboard.pressed["KeyL"]) {
+      vel = Math.max(0, vel - acc * dTime);
+    }
+
+    vel *= 1 - drag * dTime;
+
+    if (vel <= minimumVelocity) {
+      let drop = minimumVelocity - vel / 100;
+      pos[2] -= drop * heightToSpeed;
+      vel += drop;
+    }
+
+    let delta = v3.mulScalar(dir, vel * dTime);
+    vel -= delta[2] * heightToSpeed;
+    pos = v3.add(pos, delta);
+    statsDiv.innerText = `Time: ${Math.floor(time)} Position: ${pos
+      .map(n => Math.round(n))
+      .join(",")} Velocity: ${Math.floor(vel)}`;
+    orbsDiv.innerText = `Orbs: ${collectedAmount}/${orbsToCollect}`;
+    render(time, pos, dir);
+    loops++;
+    window.requestAnimationFrame(loop);
+  }
+
+  window.requestAnimationFrame(loop);
+};
---
+++ src/render.ts
@@ -0,0 +1,345 @@
+import * as twgl from "./twgl/twgl-full.js";
+import {
+  m4,
+  BufferInfo,
+  FramebufferInfo,
+  ProgramInfo,
+  VertexArrayInfo,
+  v3
+} from "./twgl/twgl-full";
+
+import { loadFile } from "./util";
+import { Vec3 } from "./twgl/v3.js";
+import { Mat4 } from "./twgl/m4.js";
+import { V2 } from "./v2.js";
+
+type PassInfo = {
+  programs: [string, string];
+  programInfo?: ProgramInfo;
+  source?: BufferInfo | VertexArrayInfo;
+  target?: FramebufferInfo;
+  overwrite?: boolean;
+  uniforms?: any;
+  then?: () => void;
+};
+
+const voxResolution = 200;
+const noiseResolution = 200;
+const toSun = v3.normalize([0, 0, 1]);
+
+export let canvas: HTMLCanvasElement;
+let gl: WebGL2RenderingContext;
+let noise: WebGLTexture;
+
+const fullScreenQuad = {
+  position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0]
+};
+
+const up = [0, 0, 1];
+
+export async function prepareRender(
+  crash: () => void,
+  collect: (arg0: number) => void,
+  collected: Uint8Array,
+  superSampling: number
+) {
+  canvas = document.getElementById("c") as HTMLCanvasElement;
+  gl = canvas.getContext("webgl2");
+
+  if (!gl) {
+    document.children[0].innerHTML =
+      "This browser has no WebGL2 support or it is not turned on.";
+    return;
+  }
+
+  /*gl.getExtension("EXT_color_buffer_float");
+  gl.getExtension("OES_texture_float_linear");
+  gl.getExtension("WEBGL_color_buffer_float");*/
+
+  twgl.addExtensionsToContext(gl);
+
+  const shaderSources = [
+    "noise-f",
+    "simple-geo-v",
+    "star-f",
+    "raymarch-f",
+    "lattice-v",
+    "geo-f",
+    "quad-v",
+    "light-f",
+    "screen-f"
+  ];
+
+  const shaderFiles = await Promise.all(
+    shaderSources.map(f => {
+      let inlined = document.getElementById(`${f}.glsl`);
+      if (inlined) return inlined.innerHTML;
+      return (
+        document.getElementById(`${f}.glsl`) || loadFile(`./shaders/${f}.glsl`)
+      );
+    })
+  );
+  const shaders = shaderFiles.reduce(
+    (p, c, i) => ((p[shaderSources[i]] = c), p),
+    {}
+  );
+
+  const [
+    noiseFs,
+    simpleGeoVs,
+    starFs,
+    raymarchFs,
+    latticeVs,
+    geoFs,
+    quadVs,
+    lightFs,
+    screenFs
+  ] = shaderFiles;
+
+  //let va = await loadStage();
+
+  const bufferWH: V2 = [
+    canvas.clientWidth * superSampling,
+    canvas.clientHeight * superSampling
+  ];
+
+  const depthTexture = createTexture(gl, bufferWH, {
+    internalFormat: gl.DEPTH24_STENCIL8
+  });
+
+  const normalTexture = createTexture(gl, bufferWH, {
+    internalFormat: gl.RGB,
+    min: gl.NEAREST
+  });
+
+  const screenTexture = createTexture(gl, bufferWH, {
+    internalFormat: gl.RGBA16F,
+    min: superSampling > 1 ? gl.LINEAR : gl.NEAREST
+  });
+
+  if (!noise) noise = makeTheNoise([quadVs, noiseFs]);
+
+  const terrainPass: PassInfo = {
+    programs: [quadVs, raymarchFs],
+    source: twgl.createBufferInfoFromArrays(gl, fullScreenQuad),
+    target: twgl.createFramebufferInfo(
+      gl,
+      [
+        { internalFormat: gl.RGBA16F },
+        { attachment: normalTexture },
+        { format: gl.DEPTH_STENCIL, attachment: depthTexture }
+      ],
+      bufferWH[0],
+      bufferWH[1]
+    )
+  };
+
+  gl.drawBuffers([gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1]);
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.BLEND);
+
+  let polyGeometry = {
+    color: { size: 1, data: [...new Array(6)].map((v, i) => 0) },
+    position: fullScreenQuad.position.map(n => n * 200 + 200)
+  };
+
+  const polyPass: PassInfo = {
+    programs: [simpleGeoVs, starFs],
+    overwrite: true,
+    source: twgl.createBufferInfoFromArrays(gl, polyGeometry),
+    target: terrainPass.target
+  };
+
+  const lightPass: PassInfo = {
+    programs: [quadVs, lightFs],
+    uniforms: {
+      u_color: terrainPass.target.attachments[0],
+      u_normal: terrainPass.target.attachments[1],
+      u_depth: terrainPass.target.attachments[2]
+    },
+    source: twgl.createBufferInfoFromArrays(gl, fullScreenQuad),
+    target: twgl.createFramebufferInfo(
+      gl,
+      [{ attachment: screenTexture }],
+      bufferWH[0],
+      bufferWH[1]
+    )
+  };
+  gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+
+  const viewportPass: PassInfo = {
+    programs: [quadVs, screenFs],
+    uniforms: {
+      u_color: lightPass.target.attachments[0],
+      u_bufferSize: bufferWH
+    },
+    source: twgl.createBufferInfoFromArrays(gl, fullScreenQuad)
+  };
+
+  twgl.resizeCanvasToDisplaySize(canvas);
+  gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  //const capturer = new CCapture( { format: 'webm' } );
+
+  let collectedBits = new Int32Array(100);
+
+  let render: (time: number, pos: Vec3, dir: Vec3) => void;
+
+  render = (time: number, eye: Vec3, direction: Vec3) => {
+    const fov = (40 * Math.PI) / 180;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const zNear = 0.5;
+    const zFar = 800;
+    const perspective = m4.perspective(fov, aspect, zNear, zFar);
+
+    const camera = m4.lookAt(eye, v3.add(eye, direction), up);
+
+    const raycastCamera = m4.lookAt([0, 0, 0], direction, up);
+    const raycastProjection = m4.inverse(
+      m4.multiply(perspective, m4.inverse(raycastCamera))
+    );
+
+    const viewTransform = m4.inverse(camera);
+    const viewProjectionTransform = m4.multiply(perspective, viewTransform);
+    const inverseViewProjectionTransform = m4.inverse(viewProjectionTransform);
+
+    //console.log(v3.subtract(m4.transformPoint(invertViewProjectionTransform, [0,1,0]), eye));
+
+    const world = m4.identity();
+
+    for (let i = 0; i < collectedBits.length; i++) collectedBits[i] = 0;
+
+    for (let i = 0; i < collected.length; i++) {
+      let j = Math.floor(i / 16);
+      if (collected[i]) collectedBits[j] = collectedBits[j] + (1 << i % 16);
+      //console.log(i, j, collectedBits);
+    }
+
+    const uniforms = {
+      "u_light[0].pos": [1300, 1000, 2000],
+      "u_light[0].color": [1, 1, 1, 1],
+      u_ambient: [1, 1, 1, 0.3],
+      u_specular: [1, 1, 1, 5],
+      u_shininess: 50,
+      u_time: time,
+      u_orbRadius: 1 + Math.sin(time * 3) * 0.2,
+      u_eye: eye,
+      u_toSun: toSun,
+      u_resolution: voxResolution,
+      u_scale: 100,
+      u_depthRange: [zNear, zFar],
+      u_bufferSize: bufferWH,
+      u_viewInverse: camera,
+      u_near: zNear,
+      u_far: zFar,
+      u_world: world,
+      u_worldInverseTranspose: m4.transpose(m4.inverse(world)),
+      u_worldViewProjection: viewProjectionTransform,
+      u_inverseWorldViewProjection: inverseViewProjectionTransform,
+      u_raycastProjection: raycastProjection,
+      u_collected: collectedBits,
+      u_noise: noise
+    };
+
+    Object.assign(lightPass.uniforms, uniforms);
+
+    polyPass.uniforms = uniforms;
+    terrainPass.uniforms = uniforms;
+
+    renderPass(gl, terrainPass);
+
+    gl.flush();
+    const data = new Float32Array(4);
+    gl.readBuffer(gl.COLOR_ATTACHMENT0);
+    gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.FLOAT, data);
+    if (data[0]) crash();
+    else if (data[1]) collect(data[1]);
+
+    //renderPass(gl, polyPass);
+
+    renderPass(gl, lightPass);
+
+    renderPass(gl, viewportPass);
+  };
+  return render;
+}
+
+function renderPass(gl: WebGL2RenderingContext, pass: PassInfo) {
+  twgl.bindFramebufferInfo(gl, pass.target);
+  if (!pass.overwrite) gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  if (!pass.programInfo) {
+    pass.programInfo = twgl.createProgramInfo(gl, pass.programs, error =>
+      console.log(error)
+    );
+  }
+  const program = pass.programInfo;
+  gl.useProgram(program.program);
+  twgl.setBuffersAndAttributes(gl, program, pass.source);
+  twgl.setUniforms(program, pass.uniforms);
+  twgl.drawBufferInfo(gl, pass.source);
+  if (pass.then) {
+    pass.then();
+  }
+}
+
+function logBuffer(
+  gl: WebGL2RenderingContext,
+  attachment = gl.COLOR_ATTACHMENT0
+) {
+  gl.flush();
+  const data = new Float32Array(100 * 100 * 1);
+  gl.readBuffer(attachment);
+  gl.readPixels(0, 0, 100, 100, gl.RED, gl.FLOAT, data);
+}
+
+function createTexture(gl: WebGL2RenderingContext, size: V2, other: Object) {
+  return twgl.createTexture(
+    gl,
+    Object.assign(
+      {
+        width: size[0],
+        height: size[1]
+      },
+      other
+    )
+  );
+}
+
+function makeTheNoise(programs: [string, string]) {
+  let numPixels = noiseResolution * noiseResolution * noiseResolution;
+  let noise2DSide = Math.ceil(Math.sqrt(numPixels));
+
+  const noisePass: PassInfo = {
+    programs,
+    source: twgl.createBufferInfoFromArrays(gl, fullScreenQuad),
+    uniforms: { u_resolution: noiseResolution, u_side: noise2DSide },
+    target: twgl.createFramebufferInfo(
+      gl,
+      [{ internalFormat: gl.R32F }],
+      noise2DSide,
+      noise2DSide
+    )
+  };
+
+  gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+
+  renderPass(gl, noisePass);
+
+  //gl.flush();
+  const data = new Float32Array(noise2DSide * noise2DSide);
+  gl.readBuffer(gl.COLOR_ATTACHMENT0);
+  gl.readPixels(0, 0, noise2DSide, noise2DSide, gl.RED, gl.FLOAT, data);
+
+  const noise3DTexture = twgl.createTexture(gl, {
+    target: gl.TEXTURE_3D,
+    width: noiseResolution,
+    height: noiseResolution,
+    depth: noiseResolution,
+    wrap: gl.MIRRORED_REPEAT,
+    minMag: gl.LINEAR,
+    internalFormat: gl.R16F,
+    src: data.slice(0, numPixels)
+  });
+
+  return noise3DTexture;
+}
